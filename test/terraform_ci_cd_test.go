041c9ad219a16fafc564ae628c842e6b3907c92a
Srikanth Arunachalam <sriaruna@WKMZT61A5F24.global.publicisgroupe.net>
2023-02-20 09:48:40 +0000

    Test 1.14

--- test/terraform_ci_cd_test.go
+++ test/terraform_ci_cd_test.go
@@ -14,9 +14,7 @@ import (
 	"github.com/gruntwork-io/terratest/modules/gcp"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/retry"
-	"github.com/gruntwork-io/terratest/modules/ssh"
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 	"github.com/stretchr/testify/assert"
 )
 
